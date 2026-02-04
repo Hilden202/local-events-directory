080c2eb8705a7040dc428393b5dda6fd1ebf6151
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-04 16:51:29 +0100

    Add IEndpoint startup registration contract

---
+++ src/server/LocalEvents.Api/Endpoints/_internal/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalEvents.Api.Endpoints._internal;
+
+// Kontrakt för registrering av endpoints vid uppstart
+public interface IEndpoint
+{
+    public static abstract void MapEndpoint(IEndpointRouteBuilder app);
+    
+}
