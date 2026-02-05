6b8ca604f9f49467d8113543f737aadb48d28ac4
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-05 08:53:42 +0100

    add get categories endpoint

---
+++ src/server/LocalEvents.Api/Endpoints/Categories/GetCategories.cs
@@ -0,0 +1,19 @@
+﻿using LocalEvents.Api.Endpoints._internal;
+
+namespace LocalEvents.Api.Endpoints.Categories;
+
+public class GetCategories : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/categories", () =>
+        {
+            return new[]
+            {
+                "Music",
+                "Sports",
+                "Tech"
+            };
+        });
+    }
+}
