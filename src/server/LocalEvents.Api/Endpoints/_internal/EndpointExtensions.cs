63a1e3753096cc9feca0f8524eff1253bc2c73a1
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-05 01:42:25 +0100

    add endpoint discovery and map endpoints at startup

---
+++ src/server/LocalEvents.Api/Endpoints/_internal/EndpointExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace LocalEvents.Api.Endpoints._internal;
+
+public static class EndpointExtensions
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder app)
+    {
+        // 1. Hämta assemblyn där IEndpoint (och alla endpoints) är definierade
+        var assembly = typeof(IEndpoint).Assembly;
+
+        // 2. Hitta alla konkreta typer som implementerar IEndpoint
+        var endpointTypes = assembly.DefinedTypes
+            .Where(x => !x.IsAbstract
+                        && !x.IsInterface
+                        && typeof(IEndpoint).IsAssignableFrom(x));
+        
+        // 3. Anropa MapEndpoint för varje endpoint-typ
+        foreach (var endpointType in endpointTypes)
+        {
+            endpointType.GetMethod(nameof(IEndpoint.MapEndpoint))!
+                .Invoke(null, new object[] { app });
+        }
+    }
+}
