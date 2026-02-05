9a1e39692f86afdf0869d27794e6b6fcb8906a76
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-05 13:49:53 +0100

    Add Category model, AppDbContext and install EF

---
+++ src/server/LocalEvents.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LocalEvents.Api.Endpoints.Categories;
+using LocalEvents.Api.Endpoints.Categories.Models;
+
+namespace LocalEvents.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories => Set<Category>();
+}
---
+++ src/server/LocalEvents.Api/Endpoints/Categories/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace LocalEvents.Api.Endpoints.Categories.Models;
+
+public class Category
+{
+    public string Name { get; set; } = default!;
+}
