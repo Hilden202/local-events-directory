db388f689728eebe472d9bd5a5683c1f4e404bb1
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-06 09:46:36 +0100

    add create event page and navigation

---
+++ src/client/src/routes/add-event/+page.svelte
@@ -0,0 +1,36 @@
+﻿<script lang="ts">
+    let title = '';
+    let categoryName = '';
+
+    async function submit() {
+        await fetch('http://localhost:5166/events', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title, categoryName})
+        });
+
+        window.location.href = '/';
+    }
+
+</script>
+
+<h1>Add event</h1>
+
+<form on:submit|preventDefault={submit}>
+    <div>
+        <label>
+            Title<br/>
+            <input bind:value={title} required/>
+        </label>
+    </div>
+
+    <div>
+        <label>
+            Category<br/>
+            <input bind:value={categoryName} required/>
+        </label>
+    </div>
+
+    <button type="submit">Create</button>
+
+</form>
