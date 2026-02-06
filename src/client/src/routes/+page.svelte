7727b304652a7277b5fee58750ae246a5c729cdf
Patrik Hildenborg Pc <phildenborg@gmail.com>
2026-02-06 09:58:39 +0100

    Initial fullstack setup with events frontend

---
+++ src/client/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let data;
+</script>
+
+<h1>Evenemang</h1>
+<a href="/add-event">
+    <button>Add Event</button>
+</a>
+
+<ul>
+    {#each data.events as event}
+        <li>
+            <strong>{event.title}</strong>
+            – {event.categoryName}
+        </li>
+    {/each}
+</ul>
